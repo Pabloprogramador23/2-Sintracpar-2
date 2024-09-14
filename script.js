736dff4ef76aab2cbcaa4142b821b0028e5da41b
Pablo <pablomagalhes@gmail.com>
2024-09-14 08:26:26 -0300

    add - campos Contactus+formulário

--- script.js
+++ script.js
@@ -30,6 +30,37 @@ function atualizarPosicaoCarrossel() {
     carrossel.style.transform = `translateX(${deslocamento}px)`; 
 } 
 
+//  Função para lidar com o envio do formulário de contato
+function enviarFormulario(event) { 
+  event.preventDefault(); // Impede o comportamento padrão do formulário (recarregar a página)
+
+  // 1. Coletar os dados do formulário
+  const nome = document.getElementById('nome').value; 
+  const email = document.getElementById('email').value;
+  const mensagem = document.getElementById('mensagem').value;  
+
+  console.log("Nome:", nome); 
+  console.log("Email:", email); 
+  console.log("Mensagem:", mensagem);
+
+  // 2.  Enviar os dados para o servidor (backend) - Faremos isso mais tarde
+  // ...  (código AJAX para enviar os dados,  quando o backend estiver pronto) 
+
+  // 3.  Exibir uma mensagem para o usuário (por enquanto,  apenas no console)
+  console.log("Formulário Enviado com Sucesso!");
+  alert("Obrigado pelo contato, entraremos em contato em breve");
+
+  // 4. Limpar os campos do formulário (opcional) 
+  document.getElementById('contatoForm').reset();
+} 
+
+//  Adiciona um evento de "submit" ao formulário 
+const formulario = document.getElementById('contatoForm'); 
+formulario.addEventListener('submit', enviarFormulario);
+
+
+
+//============================================================================
 
 function exibirNoticias() {
     const listaNoticias = document.querySelector('.lista-noticias');
